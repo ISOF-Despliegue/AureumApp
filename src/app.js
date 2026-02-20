67d37b895ea15e6de107b3acc7f632e97630664d
Mauricio Noriega <mauricionoriega.sw@outlook.com>
2026-02-20 10:25:13 -0600

    Se añadió la estructura del proyecto inicial y los primeros métodos de la API

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const reseñas = [];
+
+// Health
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "ok",
+        message: "API funcionando correctamente"
+    });
+});
+
+// Crear reseña
+app.post("/reseñas", (req, res) => {
+    const { nombreJuego, calificacion, comentario } = req.body;
+    if (!nombreJuego || !calificacion || !comentario) {
+        return res.status(400).json({
+            error: "Todos los datos son obligatorios"
+        });
+    }
+
+    const nuevaReseña = {
+        id: reseñas.length + 1,
+        nombreJuego,
+        calificacion,
+        comentario
+    };
+    reseñas.push(nuevaReseña);
+    res.status(201).json(nuevaReseña);
+});
