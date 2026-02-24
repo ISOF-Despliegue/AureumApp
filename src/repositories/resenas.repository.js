3533aed3917823be122124cce44cec018a997d5f
AbrahamC13 <canoabraham172@gmail.com>
2026-02-24 10:44:29 -0600

    CHANGE OF DB NAME

--- src/repositories/resenas.repository.js
+++ src/repositories/resenas.repository.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises');
 const path = require('path');
 
 const databaseDir = path.join(__dirname, '../../data');
-const databasePath = path.join(databaseDir, 'database.json');
+const databasePath = path.join(databaseDir, 'dbResenas.json');
 
 const leerResenas = async () => {
     try {
