31f9a29c0d1680d0a9546754bb0e9e1012cc3132
AbrahamC13 <canoabraham172@gmail.com>
2026-02-28 15:51:36 -0600

    ADD para cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const resenasRoutes = require('./routes/resenas.routes');
 const usuariosRoutes = require('./routes/usuarios.routes');
 const videojuegosRoutes = require('./routes/videojuegos.routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/health', (req, res) => {
